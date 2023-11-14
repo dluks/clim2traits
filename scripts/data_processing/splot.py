551dd91f0aaecfe8ac25dff94d4cec1f15ed7eb4
Daniel Lusk <daniel.lusk@posteo.net>
2023-11-14 06:35:00 +0100

    manually create splot grids (no geocube)

--- scripts/data_processing/splot.py
+++ scripts/data_processing/splot.py
@@ -1,77 +1,132 @@
-import json
 import logging
+import os
 from pathlib import Path
 from typing import Union
 
-import geopandas as gpd
+import numpy as np
 import pandas as pd
-import xarray as xr
-from geocube.api.core import make_geocube
-from shapely.geometry import box, mapping
+import rasterio as rio
+import rioxarray as riox
+from rasterio.transform import from_origin
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
-
-def gdf_to_grid(
-    gdf: gpd.GeoDataFrame,
-    column: str,
-    resolution: float = 0.01,
-    bbox: tuple = (-180, -60, 180, 90),
-) -> xr.DataArray:
-    """Convert a GeoDataFrame to a xarray DataArray."""
-
-    grid = make_geocube(
-        vector_data=gdf,
-        measurements=[column],
-        resolution=(-resolution, resolution),
-        geom=json.dumps(mapping(box(*bbox))),
-    )
-
-    return grid[column]
+### CONFIG
+RANGE_05 = False
 
 
-def write_grid(grid: xr.DataArray, out_path: Union[str, Path]) -> None:
-    """Write a xarray DataArray to a NetCDF file."""
+def rasterize_points(
+    points: np.ndarray, res: Union[int, float], bbox: tuple
+) -> np.ndarray:
+    """Rasterize points into a grid with a given resolution.
 
-    grid.rio.to_raster(
-        out_path,
-        dtype="float32",
+    Args:
+        points (np.ndarray): Points to rasterize, with columns (x, y, value) (for
+            geographic coordinates, use (lon, lat, value))
+        res (Union[int, float]): Resolution of the grid
+        bbox (tuple): Bounding box of the grid
+
+    Returns:
+        np.ndarray: Rasterized grid
+    """
+    width = int((bbox[2] - bbox[0]) / res)
+    height = int((bbox[3] - bbox[1]) / res)
+
+    rast = np.zeros((height, width), dtype=np.float32)
+    count_array = np.zeros_like(rast)
+    for x, y, value in points:
+        col = int((x - bbox[0]) / res)
+        row = int((bbox[3] - y) / res)
+        rast[row, col] += value
+        count_array[row, col] += 1
+
+    # Avoid division by zero
+    count_array[count_array == 0] = 1
+
+    # Calculate the average
+    rast = rast / count_array
+
+    return rast
+
+
+def write_raster(
+    raster: np.ndarray, res: Union[int, float], bbox: tuple, filename: os.PathLike
+) -> None:
+    """Write a raster to a GeoTIFF file.
+
+    Args:
+        raster (np.ndarray): Raster matrix to write
+        res (Union[int, float]): Resolution of the raster
+        bbox (tuple): Bounding box of the raster
+        filename (os.PathLike): Path to the output file
+    """
+    width = int((bbox[2] - bbox[0]) / res)
+    height = int((bbox[3] - bbox[1]) / res)
+
+    with rio.open(
+        filename,
+        "w",
+        driver="GTiff",
+        height=height,
+        width=width,
+        nodata=0,
+        count=1,
+        dtype=rio.float32,
+        crs="EPSG:4326",
+        transform=from_origin(bbox[0], bbox[3], res, res),
         compress="zstd",
-        tiled=True,
-        blockxsize=256,
-        blockysize=256,
-        predictor=2,
-        num_threads=20,
-        compute=False,
-    )
+    ) as dst:
+        dst.write(raster, 1)
 
 
 def main():
-    splot_cwm = (
-        pd.read_csv("data/gbif-splot_raw/sPlotOpen_TRYgapfilled_cwm.csv")
-        .set_index(["Longitude", "Latitude"])
-        .groupby(["Longitude", "Latitude"])
-        .mean()
-        .drop(columns=["PlotObservationID", "Releve_area"])
-        .reset_index()
+    """Convert sPlot data to raster grids."""
+    splot_cwm = pd.read_csv("data/gbif-splot_raw/sPlotOpen_TRYgapfilled_cwm.csv").drop(
+        columns=["PlotObservationID", "Releve_area"]
     )
 
-    splot_cwm = gpd.GeoDataFrame(
-        splot_cwm,
-        geometry=gpd.points_from_xy(splot_cwm.Longitude, splot_cwm.Latitude),
-        crs="EPSG:4326",
-    ).drop(columns=["Longitude", "Latitude"])
+    out_dir = Path("data/splot/0.01_deg/")
+    out_dir.mkdir(parents=True, exist_ok=True)
 
-    traits = splot_cwm.columns.difference(["geometry"])
+    traits = splot_cwm.columns.difference(["Latitude", "Longitude"])
+
+    res = 0.01
+    bbox = (-180, -60, 180, 90)
 
     for trait in traits:
         logging.info("Processing trait %s", trait)
-        grid = gdf_to_grid(splot_cwm, trait)
 
-        out_dir = Path("data/splot/0.01_deg")
-        out_dir.mkdir(parents=True, exist_ok=True)
+        points = (
+            splot_cwm[["Longitude", "Latitude", trait]].dropna(subset=[trait]).values
+        )
 
-        write_grid(grid, out_dir / f"sPlot_TRYgapfilled_{trait}_0.01deg.tif")
+        if RANGE_05:
+            out_dir = out_dir / "05_range"
+            splot_05 = riox.open_rasterio(
+                Path(
+                    "GBIF_trait_maps/global_maps/Shrub_Tree_Grass/05deg/",
+                    f"sPlot_TRYgapfilled_{trait}_05deg.grd",
+                ),
+                masked=True,
+            )
+
+            # Get min and max values of splot_05
+            min_val = splot_05.min().values.item()
+            max_val = splot_05.max().values.item()
+
+            # Drop points outside min_val and max_val, and store count of number of points dropped
+            before = points.shape[0]
+            points = points[
+                np.logical_and(points[:, 2] >= min_val, points[:, 2] <= max_val)
+            ]
+            after = points.shape[0]
+            logging.info("Dropped %d points outside range", before - after)
+        else:
+            out_dir = out_dir / "orig"
+
+        grid = rasterize_points(points, res, bbox)
+        write_raster(grid, res, bbox, out_dir / f"sPlot_{trait}_0.01deg.tif")
+
         logging.info("Processed %s", trait)
 
 
