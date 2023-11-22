a8ff3131352bd8840aed581ee64d0d869caccc52
Daniel Lusk <daniel.lusk@posteo.net>
2023-11-22 07:13:27 +0100

    fix out_dir issue; add multi-PFT processing

--- scripts/data_processing/splot.py
+++ scripts/data_processing/splot.py
@@ -9,11 +9,13 @@ import rasterio as rio
 import rioxarray as riox
 from rasterio.transform import from_origin
 
+from utils.geodata import num_to_str
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
 ### CONFIG
-RANGE_05 = False
-PFT = "Grass"
+RANGE_05 = True
+PFTs = ["Shrub_Tree_Grass", "Shrub_Tree", "Grass"]
 RES = 0.01
 BBOX = (-180, -60, 180, 90)
 
@@ -84,53 +86,67 @@ def write_raster(
 
 def main():
     """Convert sPlot data to raster grids."""
-    splot_cwm = pd.read_csv(
-        f"data/gbif-splot_raw/sPlot_cwms/sPlotOpen_TRYgapfilled_cwm{PFT}.csv"
-    ).drop(columns=["PlotObservationID", "Releve_area"])
+    for PFT in PFTs:
+        splot_cwm = pd.read_csv(
+            f"data/gbif-splot_raw/sPlot_cwms/sPlotOpen_TRYgapfilled_cwm{PFT}.csv"
+        ).drop(columns=["PlotObservationID", "Releve_area"])
 
-    out_dir = Path(f"data/splot/{PFT}/{RES:g}_deg")
-    out_dir.mkdir(parents=True, exist_ok=True)
+        parent_out = Path(f"GBIF_trait_maps/global_maps/{PFT}/{num_to_str(RES)}deg")
+        parent_out.mkdir(parents=True, exist_ok=True)
 
-    traits = splot_cwm.columns.difference(["Latitude", "Longitude"])
+        traits = splot_cwm.columns.difference(["Latitude", "Longitude"])
 
-    for trait in traits:
-        logging.info("Processing trait %s", trait)
-
-        points = (
-            splot_cwm[["Longitude", "Latitude", trait]].dropna(subset=[trait]).values
-        )
-
-        if RANGE_05:
-            out_dir = out_dir / "05_range"
-            out_dir.mkdir(parents=True, exist_ok=True)
-
-            splot_05 = riox.open_rasterio(
-                Path(
-                    f"GBIF_trait_maps/global_maps/{PFT}/05deg/",
-                    f"sPlot_TRYgapfilled_{trait}_05deg.grd",
-                ),
-                masked=True,
+        for trait in traits:
+            points = (
+                splot_cwm[["Longitude", "Latitude", trait]]
+                .dropna(subset=[trait])
+                .values
             )
 
-            # Get min and max values of splot_05
-            min_val = splot_05.min().values.item()
-            max_val = splot_05.max().values.item()
+            if RANGE_05:
+                out_dir = parent_out / "05_range"
+                out_dir.mkdir(parents=True, exist_ok=True)
 
-            # Drop points outside min_val and max_val, and store count of number of points dropped
-            before = points.shape[0]
-            points = points[
-                np.logical_and(points[:, 2] >= min_val, points[:, 2] <= max_val)
-            ]
-            after = points.shape[0]
-            logging.info("Dropped %d points outside range", before - after)
-        else:
-            out_dir = out_dir / "orig"
-            out_dir.mkdir(parents=True, exist_ok=True)
+                splot_05 = riox.open_rasterio(
+                    Path(
+                        f"GBIF_trait_maps/global_maps/{PFT}/05deg/",
+                        f"sPlot_TRYgapfilled_{trait}_05deg.grd",
+                    ),
+                    masked=True,
+                )
 
-        grid = rasterize_points(points, RES, BBOX)
-        write_raster(grid, RES, BBOX, out_dir / f"sPlot_{trait}_0.01deg.tif")
+                if isinstance(splot_05, list):
+                    raise ValueError(
+                        "Input file is a list. Please provide a single file."
+                    )
 
-        logging.info("Processed %s", trait)
+                # Get min and max values of splot_05
+                min_val = splot_05.min().values.item()
+                max_val = splot_05.max().values.item()
+
+                # Drop points outside min_val and max_val, and store count of number of
+                # points dropped
+                before = points.shape[0]
+                points = points[
+                    np.logical_and(points[:, 2] >= min_val, points[:, 2] <= max_val)
+                ]
+                after = points.shape[0]
+                logging.info("Dropped %d points outside range", before - after)
+            else:
+                out_dir = parent_out / "orig"
+                out_dir.mkdir(parents=True, exist_ok=True)
+
+            grid = rasterize_points(points, RES, BBOX)
+
+            out_fn = out_dir / f"sPlot_TRYgapfilled_{trait}_{num_to_str(RES)}deg.tif"
+            write_raster(
+                grid,
+                RES,
+                BBOX,
+                out_fn,
+            )
+
+            logging.info("Wrote %s", out_fn)
 
 
 if __name__ == "__main__":
