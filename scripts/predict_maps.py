95a6516200ee0da8eb5e8b73f69bc2dc503387be
Daniel Lusk <daniel.lusk@posteo.net>
2023-11-26 06:46:20 +0100

    improve model selection when predicting

--- scripts/predict_maps.py
+++ scripts/predict_maps.py
@@ -1,79 +1,103 @@
 import argparse
 import json
 from pathlib import Path
+from typing import Optional, Union
 
 import dask_geopandas as dgpd
 import geopandas as gpd
 import pandas as pd
 
+from utils.geodata import num_to_str
 from utils.models import Prediction, TrainedSet
 
-results = pd.read_csv("./results/training_results.csv").copy()
 
-with open("trait_id_to_trait_name.json", "r", encoding="utf-8") as f:
-    trait_map = json.load(f)
+def get_best_models(
+    model_resolution: Union[int, float],
+    model_pft: str,
+    run_ids: Optional[list[str]] = None,
+) -> tuple[list[pd.Series], list[pd.Series]]:
+    """Get the best models for each trait from the training results"""
+    results = pd.read_csv("./results/training_results.csv").copy()
 
-# First filter results to only include rows that were not imputed. Only get rows where
-# "NaN strategy" does not contain "imputed"
-results = results.loc[~results["NaN strategy"].str.contains("imputed")]
+    with open("trait_mapping.json", "r", encoding="utf-8") as f:
+        trait_map = json.load(f)
 
-# Filter results to only include rows in which Response variable starts with GBIF
-gbif_res = results.loc[results["Response variable"].str.startswith("GBIF")]
-splot_res = results.loc[results["Response variable"].str.startswith("sPlot")]
+    # First filter results to only include rows that were not imputed. Only get rows where
+    # "NaN strategy" does not contain "imputed"
+    results = results.loc[~results["NaN strategy"].str.contains("imputed")]
 
-# Filter results to only include rows in which Run ID is one of the most recent 2 run IDs
-gbif_res = gbif_res.loc[gbif_res["Run ID"].isin(gbif_res["Run ID"].unique()[-2:])]
-splot_res = splot_res.loc[splot_res["Run ID"].isin(splot_res["Run ID"].unique()[-2:])]
+    if run_ids is not None:
+        results = results.loc[results["Run ID"].isin(run_ids)]
 
-# For each trait in prim_trait_idx, get the row corresponding to its highest CV r-squared
-# Do this by finding the rows in which "Response variable" contains the trait id
-# (e.g. "_X{trait_id}_}"). Then sort by "CV r-squared" and keep the first row
-gbif_best = []
-splot_best = []
+    results = results.loc[results["Resolution"] == f"{model_resolution:g}_deg"]
+    results = results.loc[results["PFT"] == model_pft]
 
-for trait_id in trait_map.keys():
-    gbif_rows = gbif_res.loc[
-        gbif_res["Response variable"].str.contains(f"_X{trait_id}_")
-    ].sort_values(by="CV r-squared", ascending=False)
-    gbif_best_row = gbif_rows.iloc[0]
+    assert len(results) > 0, "No models found for specified resolution and PFT"
 
-    splot_rows = splot_res.loc[
-        splot_res["Response variable"].str.contains(f"_X{trait_id}_")
-    ].sort_values(by="CV r-squared", ascending=False)
-    splot_best_row = splot_rows.iloc[0]
+    gbif_res = results.loc[results["Response variable"].str.startswith("GBIF")]
+    splot_res = results.loc[results["Response variable"].str.startswith("sPlot")]
 
-    # If the best row for GBIF and sPlot are different (e.g. the best GBIF model is
-    # log-transformed and the best sPlot model is not), use the second best row (i.e.
-    # the oppositely-transformed model) for sPlot. This is because we want to compare
-    # the same model for GBIF and sPlot.
-    gbif_trait = gbif_best_row["Response variable"].split("GBIF_")[1]
-    splot_trait = splot_best_row["Response variable"].split("sPlot_")[1]
+    # For each trait in prim_trait_idx, get the row corresponding to its highest CV r-squared
+    # Do this by finding the rows in which "Response variable" contains the trait id
+    # (e.g. "_X{trait_id}_}"). Then sort by "CV r-squared" and keep the first row
+    gbif_best_rows = []
+    splot_best_rows = []
 
-    if gbif_trait != splot_trait:
-        splot_best_row = splot_rows.iloc[1]
-        splot_trait = splot_best_row["Response variable"].split("sPlot_")[1]
+    for trait_id in trait_map.keys():
+        gbif_best_row = (
+            gbif_res.loc[gbif_res["Response variable"].str.contains(f"_X{trait_id}_")]
+            .sort_values(by="CV r-squared", ascending=False)
+            .iloc[0]
+        )
 
-    # Check again to make sure gbif_best_row and splot_best_row response variables
-    # are the same.
-    assert gbif_trait == splot_trait, (
-        f"GBIF: {gbif_best_row['Response variable']}\n"
-        f"sPlot: {splot_best_row['Response variable']}"
-    )
+        splot_best_row = (
+            splot_res.loc[
+                (
+                    (splot_res["Response variable"].str.contains(f"_X{trait_id}_"))
+                    & (splot_res.Transform == gbif_best_row.Transform)
+                )
+            ]
+            .sort_values(by="CV r-squared", ascending=False)
+            .iloc[0]
+        )
 
-    gbif_best.append(gbif_best_row)
-    splot_best.append(splot_best_row)
+        # If the best row for GBIF and sPlot are different (e.g. the best GBIF model is
+        # log-transformed and the best sPlot model is not), use the second best row (i.e.
+        # the oppositely-transformed model) for sPlot. This is because we want to compare
+        # the same model for GBIF and sPlot.
+        gbif_trait_name = gbif_best_row["Response variable"].split("GBIF_")[1]
+        splot_trait_name = splot_best_row["Response variable"].split("sPlot_")[1]
+
+        assert gbif_trait_name == splot_trait_name, (
+            "Mismatched trait names:\n"
+            f"GBIF: {gbif_best_row['Response variable']}\n"
+            f"sPlot: {splot_best_row['Response variable']}"
+        )
+
+        gbif_best_rows.append(gbif_best_row)
+        splot_best_rows.append(splot_best_row)
+
+    return (gbif_best_rows, splot_best_rows)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--new", type=str)
-    parser.add_argument("--new_imputed", type=str, default=None)
+    parser.add_argument("--new-imputed", type=str, default=None)
+    parser.add_argument("-r", "--res", type=float, default=0.5, help="Model resolution")
+    parser.add_argument(
+        "-p", "--pft", type=str, default="Shrub_Tree_Grass", help="Model PFT"
+    )
+    parser.add_argument("--run-ids", nargs="+", type=str, default=None)
     parser.add_argument("--tiled", action="store_true")
     parser.add_argument("-o", "--overwrite", action="store_true")
     parser.add_argument("--test-run", action="store_true")
     parser.add_argument("--trait-ids", nargs="+", type=int, default=None)
     args = parser.parse_args()
 
-    for gbif_row, splot_row in list(zip(gbif_best, splot_best)):
+    gbif_best, splot_best = get_best_models(args.res, args.pft, args.run_ids)
+
+    for gbif_row, splot_row in zip(gbif_best, splot_best):
         # If trait_ids is not None, only predict for the specified trait ids
         if args.trait_ids is not None:
             gbif_trait_id = int(
@@ -101,13 +125,26 @@ if __name__ == "__main__":
 
         new_data = Path(args.new)
 
-        pred_dir = Path(f"./results/predictions/{new_data.stem}/{trait_name}")
+        pred_dir = Path(
+            "./results/predictions",
+            f"{num_to_str(args.res)}deg_models",
+            new_data.stem,
+            args.pft,
+            trait_name,
+        )
 
+        # TODO: Add model resolution and PFT to tiled directory pred_dir
         if args.tiled:
             pred_dir = Path(f"./results/predictions/{new_data.name}/{trait_name}")
 
         if args.test_run:
-            pred_dir = Path(f"./results/predictions/test/{new_data.name}/{trait_name}")
+            pred_dir = Path(
+                "./results/predictions/test/",
+                f"{num_to_str(args.res)}deg_models",
+                new_data.name,
+                args.pft,
+                trait_name,
+            )
 
         gbif_pred_dir = pred_dir / "GBIF"
         splot_pred_dir = pred_dir / "sPlot"
