3ced5ec4a34ab97d84f6770d698204911e63c73e
Daniel Lusk <daniel.lusk@posteo.net>
2023-12-02 06:13:22 +0100

    add overwrite arg

--- scripts/merge_predictions.py
+++ scripts/merge_predictions.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-from multiprocessing import Pool
+import multiprocessing as mp
 from pathlib import Path
 
 import dask.dataframe as dd
@@ -13,7 +13,9 @@ logging.basicConfig(
 )
 
 
-def merge_predict_tiles(trait_dir: Path, trait_set: str = "GBIF"):
+def merge_predict_tiles(
+    trait_dir: Path, trait_set: str = "GBIF", overwrite: bool = False
+):
     """Merge predictions for a given trait set.
 
     Args:
@@ -21,6 +23,16 @@ def merge_predict_tiles(trait_dir: Path, trait_set: str = "GBIF"):
         trait_set (str): Name of the trait set. Defaults to "GBIF". Must be one of
             "GBIF" or "sPlot".
     """
+    if not trait_dir.is_dir():
+        raise ValueError(f"Invalid trait directory: {trait_dir}")
+
+    if not overwrite:
+        if Path(trait_dir, f"{trait_set}/merged_predictions.parq").exists():
+            logging.info(
+                "Predictions already merged for %s %s", trait_dir.name, trait_set
+            )
+            return
+
     logging.info("Merging predictions for %s %s", trait_dir.name, trait_set)
     tiles = list(Path(trait_dir, f"{trait_set}/tiled_5x5_deg").glob("*.parq"))
     gdfs = [dgpd.read_parquet(tile, npartitions=20) for tile in tiles]
@@ -33,18 +45,33 @@ def merge_predict_tiles(trait_dir: Path, trait_set: str = "GBIF"):
     logging.info("Predictions merged for %s %s", trait_dir.name, trait_set)
 
 
-if __name__ == "__main__":
+def main():
+    """Merge tiled predictions for all traits."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "predictions", type=str, help="Path to the predictions directory."
     )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite merged predictions if they already exist.",
+    )
+    parser.add_argument("--num-procs", type=int, default=1, help="Number of processes.")
     args = parser.parse_args()
+
+    if args.num_procs == -1:
+        args.num_procs = mp.cpu_count()
 
     trait_dirs = Path(args.predictions).glob("*")
 
-    with Pool() as pool:
+    with mp.Pool(args.num_procs) as pool:
         for td in trait_dirs:
-            pool.apply_async(merge_predict_tiles, args=(td, "GBIF"))
-            pool.apply_async(merge_predict_tiles, args=(td, "sPlot"))
+            pool.apply_async(merge_predict_tiles, args=(td, "GBIF", args.overwrite))
+            pool.apply_async(merge_predict_tiles, args=(td, "sPlot", args.overwrite))
         pool.close()
         pool.join()
+
+
+if __name__ == "__main__":
+    main()
