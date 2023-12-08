393107f48f30399ea7514f548692d7856857f60e
Daniel Lusk <daniel.lusk@posteo.net>
2023-12-08 11:08:20 +0100

    add quadrant processing of tiled predictions

--- scripts/write_prediction_rasters.py
+++ scripts/write_prediction_rasters.py
@@ -1,4 +1,5 @@
 import argparse
+import gc
 import logging
 import multiprocessing
 import os
@@ -12,33 +13,37 @@ from geocube.api.core import make_geocube
 from utils.geodata import ds_to_netcdf, get_trait_name_from_data_name, num_to_str
 
 
-def pred_to_ds(
-    filename: Union[str, os.PathLike],
-    resolution: Union[int, float],
-) -> xr.Dataset:
-    """Write a prediction set to a NetCDF file."""
-    pred = gpd.read_parquet(filename)
-    pred = pred.rename(columns={"AOA": "AOA", "CoV": "COV"})
-    predictor_set = Path(filename).stem.split("_predict")[0]
-    trait_var = pred.columns[0].replace("_05deg", "") + f"_{num_to_str(resolution)}deg"
-    trait_full_name = get_trait_name_from_data_name(trait_var)
-    # only set masked_trait if it is present in the dataframe
-    if "AOA" in pred.columns:
-        masked_trait = pred.columns[4]
+def get_trait_from_gdf(gdf: gpd.GeoDataFrame, resolution: Union[float, int]) -> str:
+    """Get the trait name from a prediction GDF."""
+    return gdf.columns[0].replace("_05deg", "") + f"_{num_to_str(resolution)}deg"
+
+
+def gdf_to_final_ds(fn: Union[str, os.PathLike], resolution: Union[int, float]):
+    """Convert a prediction GDF to a final Dataset ready for publication."""
+    fn = Path(fn)
+
+    gdf = gpd.read_parquet(fn)
+    gdf = gdf.rename(columns={"CoV": "COV"})
+
+    if "AOA" in gdf.columns:
+        masked_trait = gdf.columns[4]
     else:
         masked_trait = ""
 
     ds = make_geocube(
-        vector_data=pred,
-        measurements=pred.columns.difference(["geometry", masked_trait]).tolist(),
+        gdf,
+        measurements=gdf.columns.difference(["geometry", masked_trait]).tolist(),
         resolution=(resolution, -resolution),
         output_crs="EPSG:4326",
     )
 
+    trait_var = get_trait_from_gdf(gdf, resolution)
+    trait_full_name = get_trait_name_from_data_name(trait_var)
+
     log = " (log-transformed)" if "_ln" in trait_var else ""
 
     # only include AOA and DI descriptions if they are present in the dataframe
-    if "AOA" in pred.columns and "DI" in pred.columns:
+    if "AOA" in gdf.columns and "DI" in gdf.columns:
         data_vars_description = (
             "AOA: Area of Applicability; threshold = 0.95 of DI (Meyer and Pebesma, 2021)\n"
             "COV: Coefficient of Variation\n"
@@ -57,16 +62,48 @@ def pred_to_ds(
         trait=trait_var,
         trait_description=f"{trait_full_name}{log}",
         data_vars_description=data_vars_description,
-        predictor_set=predictor_set,
         version="0.1",
+        nodata=-9999,
+    )
+
+    logging.info("Converted %s", fn.name)
+
+    return ds
+
+
+def pred_to_ds(
+    filenames: list[Union[str, os.PathLike]],
+    resolution: Union[int, float],
+    tiled: bool = False,
+    num_procs: int = 1,
+) -> xr.Dataset:
+    """Convert prediction GDF(s) into a single Dataset."""
+    if tiled:
+        predictor_set = Path(filenames[0]).parents[4].name.split("tiled_5x5_deg_")[1]
+
+        if num_procs > 1:
+            with multiprocessing.Pool(num_procs) as pool:
+                datasets = pool.starmap(
+                    gdf_to_final_ds, [(fn, resolution) for fn in filenames]
+                )
+        else:
+            datasets = [gdf_to_final_ds(fn, resolution) for fn in filenames]
+
+        logging.info("Concatenating tile datasets...")
+        ds = xr.concat(datasets, dim="y")
+    else:
+        predictor_set = Path(filenames[0]).stem.split("_predict")[0]
+        ds = gdf_to_final_ds(filenames[0], resolution)
+
+    ds = ds.assign_attrs(
+        predictor_set=predictor_set,
     )
 
     return ds
 
 
-def write_pred_ds(ds, out_dir: Union[str, os.PathLike], dry_run: bool = False):
+def write_pred_ds(ds, out_fn: Union[str, os.PathLike], dry_run: bool = False):
     """Write a prediction Dataset to a NetCDF file."""
-    out_fn = Path(out_dir, f"{ds.attrs['trait']}.nc")
 
     if dry_run:
         logging.info("Wrote %s (DRY-RUN)", out_fn)
@@ -77,15 +114,70 @@ def write_pred_ds(ds, out_dir: Union[str, os.PathLike], dry_run: bool = False):
     return
 
 
-def write_pred(
+def bounds_to_str(bounds: tuple) -> str:
+    """Convert a tuple of bounds to a string."""
+    return f"{'_'.join([str(x) for x in bounds])}"
+
+
+def pred_to_ds_and_write(
     filename: Union[str, os.PathLike],
     resolution: Union[int, float],
     out_dir: Union[str, os.PathLike],
+    tiled: bool = False,
     dry_run: bool = False,
+    num_procs: int = 1,
+    overwrite: bool = False,
 ):
     """Writes a prediction GDF to a NetCDF file."""
-    ds = pred_to_ds(filename, resolution)
-    write_pred_ds(ds, out_dir, dry_run=dry_run)
+    if tiled:
+        quadrants = [
+            {"bounds": (-180, 0, 0, 90), "tile_fns": []},
+            {"bounds": (0, 0, 180, 90), "tile_fns": []},
+            {"bounds": (-180, -90, 0, 0), "tile_fns": []},
+            {"bounds": (0, -90, 180, 0), "tile_fns": []},
+        ]
+
+        for quadrant in quadrants:
+            tile_fns = sorted(list(Path(filename).glob("*.parq")))
+            for fn in tile_fns:
+                xmin, ymin, xmax, ymax = [int(x) for x in fn.stem.split("_")[1:]]
+                if (
+                    xmin >= quadrant["bounds"][0]
+                    and ymin >= quadrant["bounds"][1]
+                    and xmax <= quadrant["bounds"][2]
+                    and ymax <= quadrant["bounds"][3]
+                ):
+                    quadrant["tile_fns"].append(fn)
+
+            trait_var = get_trait_from_gdf(
+                gpd.read_parquet(quadrant["tile_fns"][0]), resolution
+            )
+            quad_out_dir = Path(out_dir, trait_var)
+            if not dry_run:
+                quad_out_dir.mkdir(parents=True, exist_ok=True)
+
+            out_fn = Path(
+                quad_out_dir,
+                f"{trait_var}_{bounds_to_str(quadrant['bounds'])}.nc",
+            )
+
+            if not overwrite and out_fn.exists():
+                logging.info("Skipping %s (already exists)", out_fn)
+                continue
+
+            ds = pred_to_ds(quadrant["tile_fns"], resolution, tiled, num_procs)
+
+            logging.info("Writing %s...", out_fn)
+            write_pred_ds(ds, out_fn, dry_run=dry_run)
+
+            del ds
+            gc.collect()
+    else:
+        ds = pred_to_ds([filename], resolution)
+        out_fn = Path(out_dir, f"{ds.attrs['trait']}.nc")
+
+        logging.info("Writing %s...", out_fn)
+        write_pred_ds(ds, out_fn, dry_run=dry_run)
 
 
 def main():
@@ -100,7 +192,24 @@ def main():
         help="Resolution of prediction set in degrees.",
     )
     parser.add_argument(
-        "--pft", type=str, default="Shrub_Tree_Grass", help="Plant functional type."
+        "-p",
+        "--pft",
+        type=str,
+        default="Shrub_Tree_Grass",
+        help="Plant functional type.",
+    )
+    parser.add_argument(
+        "-t",
+        "--tiled",
+        action="store_true",
+        help="Tiled predictions (Should usually be combined with --num-procs > 1 if "
+        "there are more than a few tiles).",
+    )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite files if they already exist.",
     )
     parser.add_argument(
         "-v",
@@ -109,15 +218,24 @@ def main():
         help="Print verbose output.",
     )
     parser.add_argument("-d", "--dry-run", action="store_true", help="Dry run.")
-    parser.add_argument("--num-procs", type=int, default=1, help="Number of processes.")
+    parser.add_argument(
+        "-n", "--num-procs", type=int, default=1, help="Number of processes."
+    )
     args = parser.parse_args()
 
     dataset = args.dataset
-    fns = Path(dataset).glob("*/*/*.parq")
+    fns = (
+        Path(dataset).glob("*/*/tiled_5x5_deg")
+        if args.tiled
+        else Path(dataset).glob("*/*/*.parq")
+    )
     parent_dir = Path(f"maps/{args.pft}/05deg_models/{num_to_str(args.resolution)}deg")
 
     if args.verbose or args.dry_run:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        )
 
         logging.info(
             f"Processing {dataset}"
@@ -128,17 +246,37 @@ def main():
     if not args.dry_run:
         parent_dir.mkdir(parents=True, exist_ok=True)
 
-    if args.num_procs != 1:
-        if args.num_procs == -1:
-            args.num_procs = multiprocessing.cpu_count()
+    if args.num_procs == -1:
+        args.num_procs = multiprocessing.cpu_count()
+
+    if args.num_procs > 1 and not args.tiled:
         with multiprocessing.Pool(args.num_procs) as pool:
             pool.starmap(
-                write_pred,
-                [(fn, args.resolution, parent_dir, args.dry_run) for fn in fns],
+                pred_to_ds_and_write,
+                [
+                    (
+                        fn,
+                        args.resolution,
+                        parent_dir,
+                        args.tiled,
+                        args.dry_run,
+                        1,
+                        args.overwrite,
+                    )
+                    for fn in fns
+                ],
             )
     else:
         for fn in fns:
-            write_pred(fn, args.resolution, parent_dir, dry_run=args.dry_run)
+            pred_to_ds_and_write(
+                fn,
+                args.resolution,
+                parent_dir,
+                args.tiled,
+                args.dry_run,
+                args.num_procs,
+                args.overwrite,
+            )
 
     print("Done.")
 
