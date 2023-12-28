0a5d1293f5451d390a45a61585f998ec99d32100
Daniel Lusk <daniel.lusk@posteo.net>
2023-12-28 12:12:21 +0100

    add CLI and documentation

--- scripts/splot_correlations.py
+++ scripts/splot_correlations.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+import argparse
 import json
-from typing import Optional, Union
+import logging
+from typing import Union
 
 import pandas as pd
 
-from utils.geodata import splot_correlations
+from utils.spatial_stats import splot_correlations
 
 
 def create_correlation_table() -> pd.DataFrame:
-    """Create a table for storing correlations between trait predictions and their
-    corresponding sPlot grids."""
+    """
+    Create a table for storing correlations between trait predictions and their
+    corresponding sPlot grids.
+
+    Returns:
+        pd.DataFrame: The correlation table with multi-index columns and rows.
+    """
     with open("trait_mapping.json", encoding="utf-8") as f:
         trait_mapping = json.load(f)
 
@@ -25,30 +32,32 @@ def create_correlation_table() -> pd.DataFrame:
         [traits, ["Grass", "Shrub-Tree", "Shrub-Tree-Grass"]], names=["Trait", "PFT"]
     )
     df = pd.DataFrame(index=mi_rows, columns=mi_columns)
-    df.to_parquet("results/trait_correlations.parquet")
 
     return df
 
 
 def update_correlation_table(
-    df: Optional[pd.DataFrame] = None,
-    pfts: Optional[list[str]] = None,
-    resolutions: Optional[list[Union[int, float]]] = None,
+    df: pd.DataFrame,
+    pfts: list[str],
+    resolutions: list[Union[int, float]],
 ) -> pd.DataFrame:
-    """Calculate correlations between trait predictions and their corresponding sPlot
-    grids and store them in a table."""
-    if df is None:
-        df = pd.read_parquet("results/trait_correlations.parquet")
+    """
+    Calculate correlations between trait predictions and their corresponding sPlot
+    grids and store them in a table.
 
-    if pfts is None:
-        pfts = ["Grass", "Shrub_Tree", "Shrub_Tree_Grass"]
+    Args:
+        df (pd.DataFrame): The DataFrame to store the correlation values.
+        pfts (list[str]): List of plant functional types.
+        resolutions (list[Union[int, float]]): List of resolutions.
 
-    if resolutions is None:
-        resolutions = [2, 0.5, 0.2, 0.01]
+    Returns:
+        pd.DataFrame: The updated DataFrame with correlation values.
+    """
 
     for pft in pfts:
         print(f"\n{pft}")
         for resolution in resolutions:
+            logging.info("Calculating correlations for %s...", resolution)
             gbif, splot = splot_correlations(resolution, 0.5, pft)
 
             for i, trait_set in enumerate([gbif, splot]):
@@ -61,10 +70,45 @@ def update_correlation_table(
 
 
 def main():
-    """Run the script."""
+    """Main script.
+
+    This function is the entry point of the script. It parses command line arguments,
+    creates a correlation table, updates the table based on the provided arguments,
+    and saves the table to a parquet file.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "output_fn",
+        default="results/trait_correlations.parquet",
+        help="Path to output file",
+    )
+    parser.add_argument(
+        "-p",
+        "--pfts",
+        nargs="+",
+        default=["Grass", "Shrub_Tree", "Shrub_Tree_Grass"],
+        help="PFTs to include in the table.",
+    )
+    parser.add_argument(
+        "-r",
+        "--resolutions",
+        nargs="+",
+        default=[2, 0.5, 0.2, 0.01],
+        help="Resolutions to include in the table.",
+    )
+    args = parser.parse_args()
+
     df = create_correlation_table()
-    df = update_correlation_table(df)
-    df.to_parquet("results/trait_correlations.parquet")
+    df = update_correlation_table(df, args.pfts, args.resolutions)
+    df.to_parquet(args.output_fn)
 
 
 if __name__ == "__main__":
